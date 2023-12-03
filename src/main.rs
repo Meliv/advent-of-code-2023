d3d78342622c284197327b6b8ae6cf4b1e595306
Mark Thomas <18529851+Meliv@users.noreply.github.com>
2023-12-03 12:35:02 +0000

    Setting up for part 2

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod modules {
     pub mod day_2_part_1;
     pub mod day_2_part_2;
     pub mod day_3_part_1;
+    pub mod day_3_part_2;
 }
 
 use modules::day_1_part_1;
@@ -14,7 +15,8 @@ use modules::day_1_part_2;
 use modules::day_2_part_1;
 use modules::day_2_part_2;
 use modules::day_3_part_1;
+use modules::day_3_part_2;
 
 fn main() {
-    day_3_part_1::run();
+    day_3_part_2::run();
 }
