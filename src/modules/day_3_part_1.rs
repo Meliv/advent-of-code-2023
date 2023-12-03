ebfbf8b6141912e128c9c4f84adba9076df97dbf
Mark Thomas <18529851+Meliv@users.noreply.github.com>
2023-12-03 11:35:15 +0000

    Ready to do the parsing

--- src/modules/day_3_part_1.rs
+++ src/modules/day_3_part_1.rs
@@ -7,7 +7,20 @@ static REGEX_STRING: &str = r"(\d+)(\w+);*";
 pub fn run() {
     let input = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
 
+    for (i, current_line) in input.lines().peekable().enumerate() {
+
+        let prev_line = if i != 0 {
+            input.lines().nth(i-1).unwrap()
+        } else {
+            ""
+        };
+        let next_line = if i != input.lines().count() - 1 {
+            input.lines().nth(i + 1).unwrap()
+        } else {
+            ""
+        };
+    }
+
     let result: u32 = 100;
-    
     println!("Total: {result}");
 }
