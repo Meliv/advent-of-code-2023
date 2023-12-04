73a4c3ff475fa1b7dd4615f5d1b5bb1b3d2b67bc
Mark Thomas <mark.thomas@wealthify.com>
2023-12-04 18:20:05 +0000

    Not my finest work

--- src/modules/day_4_part_2.rs
+++ src/modules/day_4_part_2.rs
@@ -2,39 +2,99 @@ use regex::Regex;
 
 static INPUT_FILE_PATH: &str = "src/inputs/day_4.txt";
 
-pub fn run() -> u32 {
-    let master_list = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
-    let mut result: u32 = master_list.lines().count() as u32;
-    println!("Total: {}", result);
-
-    let mut cards_to_process: Vec<&str> = master_list.lines().collect();
-
-    while !cards_to_process.is_empty() {
-
-
-
-    }
-
-    //cards_to_process.extend(master_list.lines().skip(i+1).take(wins));
-
-
-    println!("Card To Process {:?}", cards_to_process);
-
-    println!("Total: {}", result);
-    result
+#[derive(Debug, Clone)]
+struct Game {
+    game_no: usize,
+    your_nos: Vec<u32>,
+    win_nos: Vec<u32>,
+    wins: usize,
 }
 
-fn process_card(card: &str) -> usize {
-    let game_split: Vec<&str> = card.split(':').collect();
-    
-    let game_without_line = game_split.first().unwrap();
-    let line_without_game = game_split.last().unwrap();
+impl Game {
+    fn new(card: &str) -> Game {
+        let game_split: Vec<&str> = card.split(':').collect();
 
-    let splits: Vec<&str> = line_without_game.split('|').collect();
-    let win_nos: Vec<&str> = splits.first().unwrap().trim().split(' ').filter(|c| !c.is_empty()).collect();
-    let your_nos: Vec<&str> = splits.get(1).unwrap().trim().split(' ').filter(|c| !c.is_empty()).collect();
+        let game_no: usize = game_split
+            .first()
+            .unwrap()
+            .replace("Card", "")
+            .trim()
+            .parse()
+            .unwrap();
 
-    win_nos.iter().filter(|n| your_nos.contains(n)).count()
+        let nos_split: Vec<&str> = game_split.last().unwrap().split(" | ").collect();
+
+        let win_nos_str: Vec<&str> = nos_split
+            .first()
+            .unwrap()
+            .trim()
+            .split(' ')
+            .filter(|c| !c.is_empty())
+            .collect();
+
+        let your_nos_str: Vec<&str> = nos_split
+            .last()
+            .unwrap()
+            .trim()
+            .split(' ')
+            .filter(|c| !c.is_empty())
+            .collect();
+
+        let your_nos: Vec<u32> = your_nos_str.iter().map(|n| n.parse().unwrap()).collect();
+        let win_nos: Vec<u32> = win_nos_str.iter().map(|n| n.parse().unwrap()).collect();
+        let wins = your_nos.iter().filter(|n| win_nos.contains(n)).count();
+
+        Game {
+            game_no,
+            your_nos,
+            win_nos,
+            wins
+        }
+    }
+}
+
+fn count_wins(y: Vec<u32>, w: Vec<u32>) -> usize {
+    y.iter().filter(|n| w.contains(n)).count()
+}
+
+pub fn run() -> u32 {
+    let lines = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
+    let games = get_games(lines);
+
+    let mut games_to_process = games.to_vec();
+    let mut total_games: u32 = 0;
+
+    loop {
+        let mut new_games: Vec<Game> = vec![];
+        total_games += games_to_process.len() as u32;
+
+        for card in games_to_process.iter() {
+            if card.wins != 0 {
+                let s: usize = card.game_no;
+                let e: usize = s + card.wins;
+
+                new_games.extend(games[s..e].to_vec());
+            }
+        }
+        if new_games.is_empty() {
+            break;
+        } else {
+            games_to_process = new_games;
+        }
+    }
+
+    println!("Total Games: {total_games}");
+
+    total_games
+}
+
+fn get_games(cards: String) -> Vec<Game> {
+    let mut games: Vec<Game> = vec![];
+
+    for card in cards.lines() {
+        games.push(Game::new(card));
+    }
+    games
 }
 
 #[cfg(test)]
@@ -42,7 +102,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn day4_part1_test() {
-        assert_eq!(run(), 30);
+    fn day4_part2_test() {
+        assert_eq!(run(), 5625994);
     }
 }
