a102b85e503190e064627c438a0fe3937d750b34
Mark Thomas <mark.thomas@wealthify.com>
2023-12-04 14:07:45 +0000

    Partly finished

--- src/modules/day_4_part_2.rs
+++ src/modules/day_4_part_2.rs
@@ -3,31 +3,38 @@ use regex::Regex;
 static INPUT_FILE_PATH: &str = "src/inputs/day_4.txt";
 
 pub fn run() -> u32 {
-    let input = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
-    let mut result: u32 = 0;
+    let master_list = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
+    let mut result: u32 = master_list.lines().count() as u32;
+    println!("Total: {}", result);
 
-    for current_line in input.lines() {
-        let mut line_points: u32 = 0;
-        let line_without_game = current_line.split(':').nth(1).unwrap();
-        let splits: Vec<&str> = line_without_game.split("|").collect();
-        let win_nos: Vec<&str> = splits.get(0).unwrap().trim().split(' ').filter(|c| !c.is_empty()).collect();
-        let your_nos: Vec<&str> = splits.get(1).unwrap().trim().split(' ').filter(|c| !c.is_empty()).collect();
+    let mut cards_to_process: Vec<&str> = master_list.lines().collect();
 
-        for win_no in win_nos {
-            if your_nos.contains(&win_no) {
-                if line_points == 0 {
-                    line_points += 1
-                } else {
-                    line_points *= 2
-                }
-            }
-        }
-        
-        result += line_points;
+    while !cards_to_process.is_empty() {
+
+
+
     }
+
+    //cards_to_process.extend(master_list.lines().skip(i+1).take(wins));
+
+
+    println!("Card To Process {:?}", cards_to_process);
 
     println!("Total: {}", result);
     result
+}
+
+fn process_card(card: &str) -> usize {
+    let game_split: Vec<&str> = card.split(':').collect();
+    
+    let game_without_line = game_split.first().unwrap();
+    let line_without_game = game_split.last().unwrap();
+
+    let splits: Vec<&str> = line_without_game.split('|').collect();
+    let win_nos: Vec<&str> = splits.first().unwrap().trim().split(' ').filter(|c| !c.is_empty()).collect();
+    let your_nos: Vec<&str> = splits.get(1).unwrap().trim().split(' ').filter(|c| !c.is_empty()).collect();
+
+    win_nos.iter().filter(|n| your_nos.contains(n)).count()
 }
 
 #[cfg(test)]
@@ -36,6 +43,6 @@ mod tests {
 
     #[test]
     fn day4_part1_test() {
-        assert_eq!(run(), 22193);
+        assert_eq!(run(), 30);
     }
 }
