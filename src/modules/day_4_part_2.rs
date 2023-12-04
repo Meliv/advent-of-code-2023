e102056c95a7382271b2731ea3e756e69a06f94b
Mark Thomas <mark.thomas@wealthify.com>
2023-12-04 18:36:29 +0000

    Cleanup

--- src/modules/day_4_part_2.rs
+++ src/modules/day_4_part_2.rs
@@ -2,61 +2,6 @@ use regex::Regex;
 
 static INPUT_FILE_PATH: &str = "src/inputs/day_4.txt";
 
-#[derive(Debug, Clone)]
-struct Game {
-    game_no: usize,
-    your_nos: Vec<u32>,
-    win_nos: Vec<u32>,
-    wins: usize,
-}
-
-impl Game {
-    fn new(card: &str) -> Game {
-        let game_split: Vec<&str> = card.split(':').collect();
-
-        let game_no: usize = game_split
-            .first()
-            .unwrap()
-            .replace("Card", "")
-            .trim()
-            .parse()
-            .unwrap();
-
-        let nos_split: Vec<&str> = game_split.last().unwrap().split(" | ").collect();
-
-        let win_nos_str: Vec<&str> = nos_split
-            .first()
-            .unwrap()
-            .trim()
-            .split(' ')
-            .filter(|c| !c.is_empty())
-            .collect();
-
-        let your_nos_str: Vec<&str> = nos_split
-            .last()
-            .unwrap()
-            .trim()
-            .split(' ')
-            .filter(|c| !c.is_empty())
-            .collect();
-
-        let your_nos: Vec<u32> = your_nos_str.iter().map(|n| n.parse().unwrap()).collect();
-        let win_nos: Vec<u32> = win_nos_str.iter().map(|n| n.parse().unwrap()).collect();
-        let wins = your_nos.iter().filter(|n| win_nos.contains(n)).count();
-
-        Game {
-            game_no,
-            your_nos,
-            win_nos,
-            wins
-        }
-    }
-}
-
-fn count_wins(y: Vec<u32>, w: Vec<u32>) -> usize {
-    y.iter().filter(|n| w.contains(n)).count()
-}
-
 pub fn run() -> u32 {
     let lines = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
     let games = get_games(lines);
@@ -95,6 +40,49 @@ fn get_games(cards: String) -> Vec<Game> {
         games.push(Game::new(card));
     }
     games
+}
+
+#[derive(Debug, Clone)]
+struct Game {
+    game_no: usize,
+    wins: usize,
+}
+
+impl Game {
+    fn new(card: &str) -> Game {
+        let game_split: Vec<&str> = card.split(':').collect();
+
+        let game_no: usize = game_split
+            .first()
+            .unwrap()
+            .replace("Card", "")
+            .trim()
+            .parse()
+            .unwrap();
+
+        let nos_split: Vec<&str> = game_split.last().unwrap().split(" | ").collect();
+
+        let win_nos_str: Vec<&str> = nos_split
+            .first()
+            .unwrap()
+            .split_whitespace()
+            .collect();
+
+        let your_nos_str: Vec<&str> = nos_split
+            .last()
+            .unwrap()
+            .split_whitespace()
+            .collect();
+
+        let your_nos: Vec<u32> = your_nos_str.iter().map(|n| n.parse().unwrap()).collect();
+        let win_nos: Vec<u32> = win_nos_str.iter().map(|n| n.parse().unwrap()).collect();
+        let wins = your_nos.iter().filter(|n| win_nos.contains(n)).count();
+
+        Game {
+            game_no,
+            wins,
+        }
+    }
 }
 
 #[cfg(test)]
