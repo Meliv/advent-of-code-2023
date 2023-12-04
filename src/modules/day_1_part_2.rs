12ba0ea0213fdcc3f79a55974e7abf3b57a8ad41
Mark Thomas <18529851+Meliv@users.noreply.github.com>
2023-12-04 20:59:12 +0000

    Let me cook

--- src/modules/day_1_part_2.rs
+++ src/modules/day_1_part_2.rs
@@ -6,45 +6,66 @@ static INPUT_FILE_PATH: &str = "src/inputs/day_1.txt";
 
 pub fn run() -> u32 {
     std::fs::read_to_string(INPUT_FILE_PATH)
-        .unwrap()
-        .lines()
-        .map(process_line)
-        .sum()
+    .unwrap()
+    .lines()
+    .map(process_line)
+    .sum()
 }
 
 fn process_line(line: &str) -> u32 {
-    let exp = Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine").unwrap();
+    let exp = Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine|ten").unwrap();
+    let mut start = 0;
+    let mut matches: Vec<&str> = vec![];
 
-    let first = exp.find(line).unwrap().as_str();
-    let reversed_line: String = line.chars().rev().collect();
-    let last = exp.find(reversed_line.as_str()).unwrap().as_str();
+    while let Some(captures) = exp.captures(&line[start..]) {
+        matches.push(captures.get(0).unwrap().as_str());
+        start += 1;
+    }
 
-    format!("{}{}", match_to_number(first), match_to_number(last))
-        .parse()
-        .unwrap()
-}
-
-fn match_to_number(m: &str) -> &str {
-    match m {
-        "one" => "1",
-        "two" => "2",
-        "three" => "3",
-        "four" => "4",
-        "five" => "5",
-        "six" => "6",
-        "seven" => "7",
-        "eight" => "8",
-        "nine" => "9",
-        _ => m,
+    match format!(
+        "{}{}",
+        match_to_number(matches.first().copied()),
+        match_to_number(matches.last().copied())
+    )
+    .parse()
+    {
+        Ok(parsed) => parsed,
+        _ => 0,
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn match_to_number(m: Option<&str>) -> &str {
+    let mut result = "";
 
-    #[test]
-    fn day1_part2_test() {
-        assert_eq!(run(), 54980);
+    if let Some(val) = m {
+        if val.parse::<f64>().is_ok() {
+            result = val;
+        } else {
+            result = match val {
+                "one" => "1",
+                "two" => "2",
+                "three" => "3",
+                "four" => "4",
+                "five" => "5",
+                "six" => "6",
+                "seven" => "7",
+                "eight" => "8",
+                "nine" => "9",
+                _ => ""
+            };
+        }
     }
+    result
+}
+
+fn read_input() -> Result<Vec<String>, io::Error> {
+    let file = File::open(INPUT_FILE_PATH)?;
+    let reader = BufReader::new(file);
+    let mut lines = Vec::new();
+
+    for line in reader.lines() {
+        lines.push(line?);
+    }
+
+    Ok(lines)
 }
