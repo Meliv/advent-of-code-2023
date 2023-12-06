9cb040b45df5c2c50b277c993be2ba8871462242
Mark Thomas <mark.thomas@wealthify.com>
2023-12-06 09:38:44 +0000

    Making a start

--- src/modules/day_5_part_2.rs
+++ src/modules/day_5_part_2.rs
@@ -8,78 +8,54 @@ static INPUT_FILE_PATH: &str = "src/inputs/day_5.txt";
 pub fn run() -> usize {
     let input = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
 
-    let seeds: Vec<Seed> = load_seeds(&input);
-    let reversed_maps: Vec<Map> = load_maps(input);
+    let seeds: Vec<ValueRange> = load_seeds(&input);
+    let maps: Vec<Map> = load_maps(input);
 
-    println!("{:?}", reversed_maps);
+    let result: usize = calculate(&maps, seeds);
 
-    if seeds.len() != 0 {
-        //panic!();
-
-    }
-
-    // Figure out min output for first map
-    let mut last_map = reversed_maps.first().unwrap();
-    let mut min_value: usize = last_map
-    .values
-    .iter()
-    .min_by(|x,y| x.destination_start.cmp(&y.destination_start)).unwrap()
-    .destination_start;
-
-    min_value = min_value.min(0);
-
-    // From there, iterate until you get a seed that's in your seed ranges
-    loop {
-        let result = get_seed(&reversed_maps, min_value);
-        if result.is_some() && is_in_seeds(&seeds, min_value) { break }
-        min_value += 1;
-    }
-
-    // First one you get, return it
-    println!("Result: {}", min_value);
-    min_value
+    println!("Result: {}", result);
+    result
 }
 
-fn is_in_seeds(seeds: &Vec<Seed>, value: usize) -> bool {
-    seeds.iter().any(|s| s.min <= value && s.max >= value)
-}
+fn calculate(maps: &Vec<Map>, seeds: Vec<ValueRange>) -> usize {
+    let mut value_ranges = seeds;
 
-fn get_seed(maps: &Vec<Map>, location: usize) -> Option<usize> {
-    let mut value = location;
-
-    println!("Huh? {:?}", location);
     for map in maps {
-        let x: &Vec<usize> = &map
-            .values
-            .iter()
-            //.filter(|v| value >= v.destination_start && value <= v.destination_end)
-            .map(|v| value - v.destination_start + v.source_start)
-            .collect();
-
-            if x.first().is_some() {
-                println!("{}", x.first().unwrap());
+        for map_range in &map.ranges {
+            for value_range in &value_ranges {
+                let _ = calculate_overlap(map_range, value_range);
             }
-
-
-        value = match x.first() {
-            Some(v) => *v,
-            None => return None
-        };
+        }
     }
 
-    Some(value)
+    value_ranges
+        .iter()
+        .min_by(|x, y| x.min.cmp(&y.min))
+        .unwrap()
+        .min
 }
 
-fn load_seeds(input: &str) -> Vec<Seed> {
-    let mut seeds: Vec<Seed> = vec![];
+fn calculate_overlap(map_range: &MapRange, value_range: &ValueRange) -> Overlap {
+    Overlap {
+        in_overlap: Some(ValueRange {
+            max: 0,
+            min: 0,
+            range: 0,
+        }),
+        out_overlap: None
+    }
+}
+
+fn load_seeds(input: &str) -> Vec<ValueRange> {
+    let mut seeds: Vec<ValueRange> = vec![];
     let exp = Regex::new(r"(\d+)\s+(\d+)").unwrap();
     let line = input.lines().next().unwrap();
 
     for c in exp.captures_iter(line) {
         let min: usize = c.get(1).unwrap().as_str().parse().unwrap();
         let range: usize = c.get(2).unwrap().as_str().parse().unwrap();
-        let max = range + min;
-        seeds.push(Seed { min, max });
+        let max = range + min - 1;
+        seeds.push(ValueRange { min, max, range });
     }
 
     seeds
@@ -97,7 +73,7 @@ fn load_maps(input: String) -> Vec<Map> {
 
     for b in bc {
         let x: Vec<_> = b.split('\n').collect();
-        let mut map_values: Vec<MapValue> = vec![];
+        let mut map_values: Vec<MapRange> = vec![];
 
         for y in x {
             let values: Vec<usize> = num_exp
@@ -106,41 +82,42 @@ fn load_maps(input: String) -> Vec<Map> {
                 .collect();
 
             if !values.is_empty() {
-                map_values.push(MapValue {
+                map_values.push(MapRange {
                     destination_start: *values.first().unwrap(),
-                    destination_end: *values.first().unwrap() + *values.get(2).unwrap() - 1,
                     source_start: *values.get(1).unwrap(),
-                    source_end: *values.get(1).unwrap() + *values.get(2).unwrap() - 1,
-                    offset: values.first().unwrap().abs_diff(*values.get(1).unwrap())
+                    range: *values.get(2).unwrap(),
                 });
             }
         }
 
-        maps.push(Map { values: map_values });
+        maps.push(Map { ranges: map_values });
     }
-
-    maps.reverse();
     maps
 }
 
 #[derive(Debug)]
-struct Seed {
+struct Overlap {
+    in_overlap: Option<ValueRange>,
+    out_overlap: Option<ValueRange>,
+}
+
+#[derive(Debug)]
+struct ValueRange {
     max: usize,
-    min: usize
+    min: usize,
+    range: usize,
 }
 
 #[derive(Debug)]
 struct Map {
-    values: Vec<MapValue>,
+    ranges: Vec<MapRange>,
 }
 
 #[derive(Debug)]
-struct MapValue {
+struct MapRange {
     source_start: usize,
-    source_end: usize,
     destination_start: usize,
-    destination_end: usize,
-    offset: usize
+    range: usize,
 }
 
 #[cfg(test)]
@@ -148,7 +125,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn day5_part1_test() {
-        assert_eq!(run(), 88151870);
+    fn day5_part2_test() {
+        assert_eq!(run(), 46);
     }
 }
