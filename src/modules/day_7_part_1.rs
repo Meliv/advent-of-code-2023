e53045aa5f354d420c253c233ab9ce4573f62224
Mark Thomas <mark.thomas@wealthify.com>
2023-12-07 12:55:17 +0000

    Get highest hand working

--- src/modules/day_7_part_1.rs
+++ src/modules/day_7_part_1.rs
@@ -9,25 +9,52 @@ pub fn run() -> usize {
     let input = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
 
     let hands: Vec<Hand> = input.lines().map(process_hand).collect();
-    let result: usize = hands.iter().map(|h| h.bid * h.highest_hand as usize).sum();
 
-    println!("Result: {}", result);
+    for hand in hands {
+        println!("Cards: {}, Result: {:?}", hand.cards, hand.highest_hand);
+    }
+
+
+    //let result: usize = hands.iter().map(|h| h.bid * h.highest_hand as usize).sum();
+
+    //println!("Result: {}", result);
 
     6440
 }
 
 fn process_hand(line: &str) -> Hand {
-
     let split: Vec<&str> = line.split(' ').collect();
 
+    let cards = String::from(*split.first().unwrap());
+
     Hand {
-        cards: String::from(*split.first().unwrap()),
-        highest_hand: get_highest_hand(split.first().unwrap()),
-        bid: split.get(1).unwrap().parse().unwrap()
+        highest_hand: get_highest_hand(&cards),
+        cards,
+        bid: split.get(1).unwrap().parse().unwrap(),
     }
 }
 
 fn get_highest_hand(cards: &str) -> HandType {
+    for c in cards.chars() {
+        let filtered_hand = cards.replace(c, "").to_string();
+        
+        let matching_cards: Vec<char> = cards.chars().filter(|x| x == &c).collect();
+
+        match matching_cards.len() {
+            5 => return HandType::FiveKind,
+            4 => return HandType::FourKind,
+            3 => match get_highest_hand(&filtered_hand) {
+                HandType::OnePair => return HandType::FullHouse,
+                _ => return HandType::ThreeKind,
+            },
+            2 => match get_highest_hand(&filtered_hand) {
+                HandType::ThreeKind => return HandType::FullHouse,
+                HandType::OnePair => return HandType::TwoPair,
+                _ => return HandType::OnePair,
+            },
+            _ => {}
+        }
+    }
 
     HandType::HighCard
 }
@@ -36,7 +63,7 @@ fn get_highest_hand(cards: &str) -> HandType {
 struct Hand {
     cards: String,
     highest_hand: HandType,
-    bid: usize
+    bid: usize,
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -47,7 +74,7 @@ enum HandType {
     ThreeKind = 4,
     FullHouse = 5,
     FourKind = 6,
-    FiveKind = 7
+    FiveKind = 7,
 }
 
 #[cfg(test)]
