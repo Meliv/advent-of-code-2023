eaf7fe8a23d16b314778173a0a96a89ef37c2557
Mark Thomas <mark.thomas@wealthify.com>
2023-12-07 14:06:13 +0000

    Day 7 Part 2 setup

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ mod modules {
     pub mod day_5_part_1;
     pub mod day_5_part_2;
     pub mod day_7_part_1;
+    pub mod day_7_part_2;
 }
 
 use modules::day_1_part_1;
@@ -25,8 +26,8 @@ use modules::day_4_part_1;
 use modules::day_4_part_2;
 use modules::day_5_part_1;
 use modules::day_5_part_2;
-use modules::day_7_part_1;
+use modules::day_7_part_2;
 
 fn main() {
-    day_7_part_1::run();
+    day_7_part_2::run();
 }
