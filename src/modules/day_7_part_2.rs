22b365be4433a763b0e5c4ccd506ccc012895fa1
Mark Thomas <mark.thomas@wealthify.com>
2023-12-07 17:40:18 +0000

    Ship it

--- src/modules/day_7_part_2.rs
+++ src/modules/day_7_part_2.rs
@@ -1,5 +1,7 @@
 use core::num;
 use itertools::Itertools;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
 use regex::Regex;
 
@@ -11,9 +13,9 @@ pub fn run() -> usize {
     let hands: Vec<Hand> = input.lines().map(process_hand).collect();
 
     let hands_grouped_by_type = hands
-    .into_iter()
-    .sorted_by_key(|x| x.highest_hand)
-    .group_by(|h| h.highest_hand);
+        .into_iter()
+        .sorted_by_key(|x| x.highest_hand)
+        .group_by(|h| h.highest_hand);
 
     let mut sorted_hands: Vec<Hand> = vec![];
 
@@ -42,6 +44,7 @@ fn process_hand(line: &str) -> Hand {
     Hand {
         highest_hand: get_highest_hand(&cards),
         cards: translate_cards(&cards),
+        untranslated_cards: cards,
         bid: split.get(1).unwrap().parse().unwrap(),
     }
 }
@@ -51,11 +54,63 @@ fn translate_cards(cards: &str) -> String {
         .replace('A', "Z")
         .replace('K', "Y")
         .replace('Q', "X")
-        .replace('J', "W")
+        .replace('J', "0")
         .replace('T', "V")
 }
 
 fn get_highest_hand(cards: &str) -> HandType {
+    let joker_count: usize = cards.chars().filter(|c| c == &'J').count();
+
+    match joker_count {
+        0 => get_hand_type(cards),
+        _ => get_joker_hand_type(cards, joker_count),
+    }
+}
+
+fn get_joker_hand_type(cards: &str, joker_count: usize) -> HandType {
+    let x = cards.replace('J', "");
+    let filtered_cards = x.as_str();
+    let unique_chars_hashset: HashSet<char> = filtered_cards.chars().collect();
+    let unique_chars = unique_chars_hashset.len();
+
+    match joker_count {
+        5 => HandType::FiveKind,
+        4 => HandType::FiveKind,
+        // 3 cards could be Full House (JJJAA), FourKind (JJJA2)
+        3 => match unique_chars {
+            1 => HandType::FiveKind,
+            _ => HandType::FourKind,
+        },
+        // 2 cards could be Five Kind (JJAAA), Two Pair (JJA22), Three Kind (JJA23)
+        2 => match unique_chars {
+            1 => HandType::FiveKind,
+            2 => HandType::FourKind,
+            _ => HandType::ThreeKind,
+        },
+        // J3JAA
+        // 1 card could be FiveKind (JAAAA), FourKind (JA222) FullHouse (JAA22), ThreeKind (JA233), Pair (JA234)
+        1 => match unique_chars {
+            1 => HandType::FiveKind,
+            2 => {
+                let mut char_counts: HashMap<char, usize> = HashMap::new(); // ChatGPT written all over this
+                for ch in filtered_cards.chars() {
+                    *char_counts.entry(ch).or_insert(0) += 1;
+                }
+
+                let f = char_counts.iter().next().unwrap();
+                match f.1 {
+                    2 => HandType::FullHouse,
+                    _ => HandType::FourKind,
+                }
+            }
+            3 => HandType::ThreeKind,
+            _ => HandType::OnePair,
+        },
+        _ => HandType::HighCard, // Never hit
+    }
+}
+
+fn get_hand_type(cards: &str) -> HandType {
     for c in cards.chars() {
         let filtered_hand = cards.replace(c, "").to_string();
 
@@ -83,6 +138,7 @@ fn get_highest_hand(cards: &str) -> HandType {
 #[derive(Debug)]
 struct Hand {
     cards: String,
+    untranslated_cards: String,
     highest_hand: HandType,
     bid: usize,
 }
@@ -104,6 +160,6 @@ mod tests {
 
     #[test]
     fn day7_part2_test() {
-        assert_eq!(run(), 5905);
+        assert_eq!(run(), 248747492);
     }
 }
