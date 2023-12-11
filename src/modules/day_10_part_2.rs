6021f1ba8c4cbae8e5e0deed2684d0e86f7a637d
Mark Thomas <18529851+Meliv@users.noreply.github.com>
2023-12-11 10:44:13 +0000

    Done but this is some of the worst code ever. Needs a solid refactor

--- src/modules/day_10_part_2.rs
+++ src/modules/day_10_part_2.rs
@@ -6,9 +6,10 @@ use num::complex::ComplexFloat;
 const INPUT_FILE_PATH: &str = "src/inputs/day_10.txt";
 const START_REGEX: &str = "S";
 
-fn replace_loop_pipe(input: &mut Vec<Vec<char>>) {
+fn replace_loop_pipe(input: &mut Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut filtered_map = input.clone();
     let mut start: (isize, isize) = (0, 0);
-    for (y, col) in input.iter().enumerate() {
+    for (y, col) in filtered_map.iter().enumerate() {
         for (x, c) in col.iter().enumerate() {
             if c == &'S' {
                 start = (x as isize, y as isize);
@@ -16,7 +17,7 @@ fn replace_loop_pipe(input: &mut Vec<Vec<char>>) {
         }
     }
 
-    let start_direction = get_starting_direction(&input, start);
+    let start_direction = get_starting_direction(&filtered_map, start);
 
     // Mutable in the loop for pathfinding
     let mut current_tile: char = start_direction.0;
@@ -73,10 +74,11 @@ fn replace_loop_pipe(input: &mut Vec<Vec<char>>) {
 
         last_pos = current_pos;
         current_pos = next_pos;
-        current_tile = get_x_y(&input, current_pos.0, current_pos.1);
+        current_tile = get_x_y(&filtered_map, current_pos.0, current_pos.1);
         // Part 2
-        set_x_y(input, last_pos.0, last_pos.1, 'S');
+        set_x_y(&mut filtered_map, last_pos.0, last_pos.1, 'S');
     }
+    filtered_map
 }
 
 pub fn run() -> usize {
@@ -92,58 +94,18 @@ pub fn run() -> usize {
                 .collect_vec(),
         );
     }
-    let col_count: usize = string_input.lines().next().unwrap().len();
 
-    replace_loop_pipe(&mut map);
+    let filtered_map = replace_loop_pipe(&mut map);
 
-    let mut yyy: Option<(isize, isize)> = None;
-    for (i_y, row) in map.iter().enumerate() {
-        let mut in_loop: bool = row.first().unwrap() == &'S';
-
-        for (i_x, chunk) in row.chunks(2).enumerate() {
-            match chunk {
-                ['S', 'S'] => {}
-                [_, 'S'] => in_loop = true,
-                ['S', _] => {
-                    if in_loop {
-                        in_loop = false
-                    } else {
-                        yyy = Some((i_x as isize, i_y as isize))
-                    }
-                }
-                [_, _] => {
-                    if in_loop {
-                        yyy = Some((i_x as isize, i_y as isize))
-                    }
-                }
-                _ => unreachable!(),
-            }
+    for x in &filtered_map {
+        for c in x {
+            print!("{}", c);
         }
-
-        if yyy.is_some() {
-            break;
-        }
+        println!();
     }
+    println!();
 
-    for (i_y, row) in map.iter_mut().enumerate() {
-        for (i_x, c) in row.iter().enumerate() {
-            let ray_count:(isize, isize) = shoot_ray(map, i_x as isize, i_y as isize);
-            if ray_count.0 % 2 == 1 && ray_count.1 % 2 == 1{
-                set_x_y(&mut map, i_x as isize, i_y as isize, 'X');
-            }
-        }
-    }
-    //let test_x_y = (1, 1); // For testing
-    /*
-    flood_fill(
-        &mut map,
-        col_count,
-        row_count,
-        yyy.unwrap().0,
-        yyy.unwrap().1,
-    );
-
-    */
+    let result = ray_count(&map, &filtered_map);
 
     for x in &map {
         for c in x {
@@ -152,73 +114,92 @@ pub fn run() -> usize {
         println!();
     }
 
-    0
+    println!("Result: {}", result);
+
+    result
 }
 
-fn shoot_ray(map: &mut Vec<Vec<char>>, i_x: isize, i_y: isize) -> (isize, isize) {
+fn ray_count(map: &Vec<Vec<char>>, filtered_map: &Vec<Vec<char>>) -> usize {
+    let mut match_count = 0;
+    for (i_y, row) in filtered_map.iter().enumerate() {
+        for (i_x, c) in row.iter().enumerate() {
 
-    (0,0)
-}
+            if c == &'S'{
+                continue;
+            }
+            //           1111111111
+            // 01234567890123456789
+            // L--J.L7.X.LJS7F-7L7.
+            let mut x_count = i_x as isize + 1;
+            let mut north_count = 0;
+            let mut south_count = 0;
+            while x_count < row.len() as isize {
+                let char = get_x_y(map, x_count, i_y as isize);
+                if get_x_y(filtered_map, x_count, i_y as isize) != 'S' {
+                    x_count += 1;
+                    continue;
 
+                }
+                match char {
+                    '|' => north_count += 1,
+                    'L' => north_count += 1,
+                    'J' => north_count += 1,
+                    _ => {}
+                }
 
-fn flood_fill(input: &mut Vec<Vec<char>>, row_count: usize, col_count: usize, x: isize, y: isize) {
-    let mut queue: VecDeque<(isize, isize)> = VecDeque::new();
-    let mut p: (isize, isize) = (x, y);
-    queue.push_back(p);
+                match char {
+                    '7' => south_count += 1,
+                    '|' => south_count += 1,
+                    'F' => south_count += 1,
+                    'S' => south_count += 1,
+                    _ => {}
+                }
 
-    set_x_y(input, x, y, 'X');
+                //
 
-    while queue.len() > 0 {
-        let current = queue.pop_front().unwrap();
+                x_count += 1;
+            }
+            let odd_right = north_count > 0 && south_count > 0 && north_count % 2 == 1 && south_count % 2 == 1;
 
-        // East
-        if is_valid(input, row_count, col_count, current.0 + 1, current.1) {
-            set_x_y(input, current.0 + 1, current.1, 'X');
-            p = (current.0 + 1, current.1);
-            queue.push_back(p);
-        }
+            // 0123456789
+            // OOOOOXOOOO
+            x_count = i_x as isize - 1;
+            north_count = 0;
+            south_count = 0;
 
-        // West
-        if is_valid(input, row_count, col_count, current.0 - 1, current.1) {
-            set_x_y(input, current.0 - 1, current.1, 'X');
-            p = (current.0 - 1, current.1);
-            queue.push_back(p);
-        }
+            while x_count >= 0 {
+                let char = get_x_y(map, x_count, i_y as isize);
+                if get_x_y(filtered_map, x_count, i_y as isize) != 'S' {
+                    x_count -= 1;
+                    continue;
+                }
+                match char {
+                    '|' => north_count += 1,
+                    'L' => north_count += 1,
+                    'J' => north_count += 1,
+                    _ => {}
+                }
 
-        // North
-        if is_valid(input, row_count, col_count, current.0, current.1 - 1) {
-            set_x_y(input, current.0, current.1 - 1, 'X');
-            p = (current.0, current.1 - 1);
-            queue.push_back(p);
-        }
+                match char {
+                    '7' => south_count += 1,
+                    '|' => south_count += 1,
+                    'F' => south_count += 1,
+                    'S' => south_count += 1,
+                    _ => {}
+                }
 
-        // South
-        if is_valid(input, row_count, col_count, current.0, current.1 + 1) {
-            set_x_y(input, current.0, current.1 + 1, 'X');
-            p = (current.0, current.1 + 1);
-            queue.push_back(p);
+                x_count -= 1;
+            }
+            let odd_left = north_count > 0 && south_count > 0 && north_count % 2 == 1 && south_count % 2 == 1;
+
+            if odd_left && odd_right {
+                println!("match at x {} y {}", i_x, i_y);
+                match_count += 1;
+            }
         }
     }
-}
 
-fn is_valid(
-    input: &mut Vec<Vec<char>>,
-    row_count: usize,
-    col_count: usize,
-    x: isize,
-    y: isize,
-) -> bool {
-    if x < 0
-        || x as usize >= row_count
-        || y < 0
-        || y as usize >= col_count
-        || get_x_y(input, x, y) == 'S'
-        || get_x_y(input, x, y) == 'X'
-    {
-        return false;
-    }
-
-    true
+    match_count
 }
 
 fn get_x_y(input: &Vec<Vec<char>>, x: isize, y: isize) -> char {
@@ -269,6 +250,6 @@ mod tests {
 
     #[test]
     fn day10_part2_test() {
-        assert_eq!(run(), 4);
+        assert_eq!(run(), 541);
     }
 }
