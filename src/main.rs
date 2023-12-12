e60a0b1d137ccf2f8a04fa36aaa4cfff2415ccc0
Mark Thomas <18529851+Meliv@users.noreply.github.com>
2023-12-12 19:04:06 +0000

    Setup for day 12

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ mod modules {
     pub mod day_10_part_2;
     pub mod day_11_part_1;
     pub mod day_11_part_2;
+    pub mod day_12_part_1;
 }
 
 use modules::day_1_part_1;
@@ -48,7 +49,8 @@ use modules::day_10_part_1;
 use modules::day_10_part_2;
 use modules::day_11_part_1;
 use modules::day_11_part_2;
+use modules::day_12_part_1;
 
 fn main() {
-    day_11_part_2::run();
+    day_12_part_1::run();
 }
