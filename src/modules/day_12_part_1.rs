a9f5c20d36aa4e7a3c9890df237d13108df5809b
Mark Thomas <18529851+Meliv@users.noreply.github.com>
2023-12-14 01:56:12 +0000

    Same position as yesterday but the code is slightly better

--- src/modules/day_12_part_1.rs
+++ src/modules/day_12_part_1.rs
@@ -10,11 +10,11 @@ pub fn run() -> usize {
 
     let test_data: Vec<(String, Vec<usize>)> = vec![
         //(String::from("???.###"), vec![1,1,3]), // Expect 1, Actual 1
-        (String::from(".??..??...?##."), vec![1, 1, 3]), // Expect 4, Actual 4
-                                                         //(String::from("?#?#?#?#?#?#?#?"), vec![1, 3, 1, 6]), // Expect 1, Actual 1
-                                                         //(String::from("????.#...#..."), vec![4,1,1]), // Expect 1, Actual 1
-                                                         //(String::from("????.######..#####."), vec![1,6,5]), // Expect 4, Actual 2
-                                                         //(String::from("?###????????"), vec![3,2,1]), // Expect 10, Actual 11
+        //(String::from(".??..??...?##."), vec![1, 1, 3]), // Expect 4, Actual 4
+        //(String::from("?#?#?#?#?#?#?#?"), vec![1, 3, 1, 6]), // Expect 1, Actual 1
+        //(String::from("????.#...#..."), vec![4,1,1]), // Expect 1, Actual 1
+        (String::from("????.######..#####."), vec![1,6,5]), // Expect 4, Actual 2
+        (String::from("?###????????"), vec![3,2,1]), // Expect 10, Actual 11
     ];
 
     //?#?#?#?#?#?#?#?
@@ -54,32 +54,34 @@ impl SpringField {
 
     fn calculate_permutations(&mut self, input: String, mut start: usize, groups: Vec<usize>) {
         
+        
+        // Edge case city
         if groups.is_empty(){
             return;
         }
-
+        
         while start < input.len()
         {
             let mut updated_string_vec: Vec<char> = input.chars().collect();
+            
+            if updated_string_vec[start] == '.' {
+                return;
+            }
+
             // Find end of group (you already know start)
             let end = match input
                 .chars()
                 .enumerate()
                 .find(|(i, c)| i > &start && c == &'.')
             {
-                Some(n) => n.0,
+                Some(n) => n.0 - 1,
                 None => input.len() - 1,
             };
 
             let g = *groups.first().unwrap();
 
             // Do the insert
-            if end - start >= g { // Has to be #1 because .###./.???. with 3 resolves to 2, not correct
-
-                // Hack
-                if updated_string_vec[start+g] == '.' {
-                    return;
-                }
+            if (end - start + 1) >= g { // Has to be #1 because .###./.???. with 3 resolves to 2, not correct
 
                 updated_string_vec[start..start + g].fill('#');
 
