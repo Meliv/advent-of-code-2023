ed05dd681e4a1ca8dbd14cc6b2aaa05274027384
Mark Thomas <18529851+Meliv@users.noreply.github.com>
2023-12-16 01:40:23 +0000

    Day 15 Part 2

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ mod modules {
     pub mod day_13_part_2;
     pub mod day_14_part_1;
     pub mod day_15_part_1;
+    pub mod day_15_part_2;
 }
 
 use modules::day_1_part_1;
@@ -60,7 +61,8 @@ use modules::day_13_part_1;
 use modules::day_13_part_2;
 use modules::day_14_part_1;
 use modules::day_15_part_1;
+use modules::day_15_part_2;
 
 fn main() {
-    day_15_part_1::run();
+    day_15_part_2::run();
 }
---
+++ src/modules/day_15_part_2.rs
@@ -0,0 +1,136 @@
+use std::{
+    collections::{HashMap, VecDeque},
+    iter,
+    thread::current, array::IntoIter,
+};
+
+use itertools::Itertools;
+
+const INPUT_FILE_PATH: &str = "src/inputs/day_15.txt";
+
+pub fn run() -> usize {
+    let input = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
+    let x: String = input.replace(['\r', '\n'], "");
+    let replaced_input: Vec<&str> = x.split(',').collect();
+    
+    let mut map: Map = Map::new();
+    for v in replaced_input {
+        map.process(v);
+    }
+    
+    let result = map.get_result();
+    
+    println!("Result: {}", result);
+    result
+}
+
+fn calculate_string(input: &str) -> usize {
+    let mut result = 0;
+    for c in input.chars() {
+        result += c as usize;
+        result *= 17;
+        result %= 256;
+    }
+    result
+}
+
+#[derive(Debug)]
+struct Map {
+    boxes: Vec<LensBox>,
+}
+
+impl Map {
+    fn new() -> Map {
+        let mut boxes: Vec<LensBox> = Vec::with_capacity(256);
+        for _ in 0..256 {
+            boxes.push(LensBox::new());
+        } 
+        Map { boxes }
+    }
+    
+    fn get_result(&self) -> usize {
+        let mut result = 0;
+        for (i, b) in self.boxes.iter().enumerate() {
+            result += (i+1) * b.get_result();
+        }
+        result
+    }
+    
+    fn process(&mut self, value: &str) {
+        match value.contains('=') {
+            true => self.add_lens(value),
+            _ => self.remove_lens(value)
+        };
+    }
+    
+    fn add_lens(&mut self, value: &str) {
+        let split_values: Vec<&str> = value.split('=').collect();
+        let label = *split_values.first().unwrap();
+        let lens_value: usize = split_values.get(1).unwrap().parse().unwrap();
+        let box_index = calculate_string(label);
+
+        self.boxes.get_mut(box_index).unwrap().add_lens(label, lens_value);
+    }
+
+    fn remove_lens(&mut self, value: &str) {
+        let label: &str = &value[0..value.len()-1];
+        let box_index = calculate_string(label);
+
+        self.boxes.get_mut(box_index).unwrap().remove_lens(label);
+    }
+}
+
+#[derive(Debug)]
+struct LensBox {
+    lenses: VecDeque<Lens>,
+}
+
+impl LensBox {
+    fn new() -> LensBox {
+        LensBox {
+            lenses: VecDeque::new(),
+        }
+    }
+
+    fn add_lens(&mut self, label: &str, value: usize) {
+
+        let lens = self.lenses.iter_mut().find(|l| l.label == label);
+
+        match lens {
+            Some(l) => { l.value = value },
+            None => {self.lenses.push_back(Lens { label: label.to_string(), value })}
+        }
+    }
+
+    fn remove_lens(&mut self, label: &str) {
+        let lens = self.lenses.iter().find_position(|l| l.label == label);
+        
+        if let Some(l) = lens {
+            self.lenses.remove(l.0);
+        }
+    }
+
+    fn get_result(&self) -> usize {
+        let mut result = 0;
+        for (i, lens) in self.lenses.iter().enumerate() {
+            result += (1 + i) * lens.value;
+        }
+        result
+    }
+}
+
+#[derive(Debug)]
+struct Lens {
+    label: String,
+    value: usize,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day15_part1_test() {
+        assert_eq!(run(), 136);
+    }
+}
