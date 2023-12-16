126e19cb3ce1286e04a18814cd164a472d50e887
Mark Thomas <18529851+Meliv@users.noreply.github.com>
2023-12-16 00:19:54 +0000

    Day 15 Part 1

--- src/modules/day_15_part_1.rs
+++ src/modules/day_15_part_1.rs
@@ -1,24 +1,28 @@
-use std::iter;
+use std::{iter, thread::current};
 
 use itertools::Itertools;
 
 const INPUT_FILE_PATH: &str = "src/inputs/day_15.txt";
 
 pub fn run() -> usize {
-    //let input = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
-    let mut result = 0;
+    let input = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
+    let x: String = input.replace(['\r','\n'], "");
+    let replaced_input: Vec<&str> = x.split(',').collect();
 
-
-    let test = "rn=1";
-
-    for c in test.chars() {
-
-        println!("{}", c);
-
-    }
-
+    let result_vec: Vec<usize> = replaced_input.iter().map(|s| calculate_string(s)).collect();
+    let result = result_vec.iter().sum();
 
     println!("Result: {}", result);
+    result
+}
+
+fn calculate_string(input: &str) -> usize {
+    let mut result = 0;
+    for c in input.chars() {
+        result += c as usize;
+        result *= 17;
+        result %= 256;
+    }
     result
 }
 
