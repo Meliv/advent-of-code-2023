2f2e3fcf03e209cae6ad5115736c86e42295ae41
Mark Thomas <18529851+Meliv@users.noreply.github.com>
2023-12-17 00:14:09 +0000

    Midway through the refactor

--- src/modules/day_16_part_1.rs
+++ src/modules/day_16_part_1.rs
@@ -8,38 +8,43 @@ use itertools::Itertools;
 const INPUT_FILE_PATH: &str = "src/inputs/day_16.txt";
 
 pub fn run() -> usize {
-    let input = std::fs::read_to_string(INPUT_FILE_PATH)
-        .unwrap()
-        .replace(['\r', '\n'], "");
-
-    let first_beam = match input.chars().next().unwrap() {
-        '.' => Beam {
-            current_tile_char: '.',
-            current_tile_index: 0,
-            direction: BeamDirection::East,
-        },
-        '\\' => Beam {
-            current_tile_char: '\\',
-            current_tile_index: 0,
-            direction: BeamDirection::South,
-        },
-        _ => panic!(),
-    };
-
-    let mut beams: Vec<Beam> = vec![first_beam];
+    let input = std::fs::read_to_string(INPUT_FILE_PATH).unwrap();
 
     let map = Map::new(input);
 
-    let mut energised_cells: HashSet<isize> = HashSet::new();
-    energised_cells.insert(0);
+    let initial_beam = Beam {
+        current_tile_char: input.chars().next().unwrap(),
+        current_tile_index: 0,
+        direction: match input.chars().next().unwrap() {
+            '.' => BeamDirection::East,
+            '\\' => BeamDirection::South,
+        },
+    };
+
+    let mut beams: Vec<Beam> = vec![initial_beam];
+
+    let mut energised_cells: HashSet<EnergisedCell> = HashSet::new();
+    energised_cells.insert(EnergisedCell {
+        x: 0,
+        y: 0,
+        entry_direction: initial_beam.direction,
+    });
 
     while !beams.is_empty() {
-        let beam = beams.get(0).unwrap();
+        let beam = beams.first().unwrap();
 
-        let next_beam = map.get_next_beams(beam);
+        let next_beams = map.get_next_beams(beam);
 
-        if let Some(bs) = next_beam {
-            energised_cells.insert(bs.first().unwrap().current_tile_index);
+        if !next_beams.is_empty() {
+
+            let b = next_beams.first().unwrap();
+
+            /*
+            let ec = EnergisedCell {
+            }
+            */
+
+            energised_cells.insert(next_beams.first().unwrap().current_tile_index);
             beams.extend(bs);
         }
 
@@ -67,7 +72,7 @@ struct Beam {
     direction: BeamDirection,
 }
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
 enum BeamDirection {
     // 10 chars long in test input
     North = -110,
@@ -77,16 +82,34 @@ enum BeamDirection {
 }
 
 struct Map {
-    tiles: String,
+    cells: Vec<Cell>,
+}
+struct Cell {
+    x: usize,
+    y: usize,
+    c: char,
+}
+
+#[derive(Eq, PartialEq, Hash)]
+struct EnergisedCell {
+    x: usize,
+    y: usize,
+    entry_direction: BeamDirection,
 }
 
 impl Map {
     fn new(input: String) -> Map {
-        Map {
-            tiles: input.clone(),
+        let mut cells: Vec<Cell> = vec![];
+        for (i_y, line) in input.lines().enumerate() {
+            for (i_x, c) in line.chars().enumerate() {
+                cells.push(Cell { x: i_x, y: i_y, c })
+            }
         }
+
+        Map { cells }
     }
 
+    /*
     fn print(&self, tiles: &HashSet<isize>) {
         let mut x: Vec<char> = self.tiles.chars().collect();
         for (i, c) in x.iter().enumerate() {
@@ -102,8 +125,13 @@ impl Map {
         }
         println!();
     }
+    */
 
-    fn get_next_beams(&self, beam: &Beam) -> Option<Vec<Beam>> {
+    fn get_tile(&self, x: usize, y: usize) -> Cell {
+        *self.cells.iter().find(|c| c.x == x && c.y == y).unwrap()
+    }
+
+    fn get_next_beams(&self, beam: &Beam) -> Vec<Beam> {
         // Need to make sure the beam can't wrap to the previous line
         let line_start: isize = (beam.current_tile_index / 110) * 110;
         let line_end: isize = line_start + 109;
@@ -113,7 +141,7 @@ impl Map {
         if (beam.direction == BeamDirection::East || beam.direction == BeamDirection::West)
             && (current_tile_index < line_start || current_tile_index > line_end)
         {
-            return None;
+            return vec![];
         }
 
         match current_tile_index < 0 || current_tile_index >= self.tiles.len() as isize {
@@ -121,49 +149,49 @@ impl Map {
                 let current_tile_char =
                     self.tiles.chars().nth(current_tile_index as usize).unwrap();
                 match (current_tile_char, beam.direction) {
-                    ('/', BeamDirection::East) => Some(vec![Beam {
+                    ('/', BeamDirection::East) => vec![Beam {
                         current_tile_char,
                         current_tile_index,
                         direction: BeamDirection::North,
-                    }]),
-                    ('/', BeamDirection::South) => Some(vec![Beam {
+                    }],
+                    ('/', BeamDirection::South) => vec![Beam {
                         current_tile_char,
                         current_tile_index,
                         direction: BeamDirection::West,
-                    }]),
-                    ('/', BeamDirection::West) => Some(vec![Beam {
+                    }],
+                    ('/', BeamDirection::West) => vec![Beam {
                         current_tile_char,
                         current_tile_index,
                         direction: BeamDirection::South,
-                    }]),
-                    ('/', BeamDirection::North) => Some(vec![Beam {
+                    }],
+                    ('/', BeamDirection::North) => vec![Beam {
                         current_tile_char,
                         current_tile_index,
                         direction: BeamDirection::East,
-                    }]),
+                    }],
 
-                    ('\\', BeamDirection::East) => Some(vec![Beam {
+                    ('\\', BeamDirection::East) => vec![Beam {
                         current_tile_char,
                         current_tile_index,
                         direction: BeamDirection::South,
-                    }]),
-                    ('\\', BeamDirection::South) => Some(vec![Beam {
+                    }],
+                    ('\\', BeamDirection::South) => vec![Beam {
                         current_tile_char,
                         current_tile_index,
                         direction: BeamDirection::East,
-                    }]),
-                    ('\\', BeamDirection::West) => Some(vec![Beam {
+                    }],
+                    ('\\', BeamDirection::West) => vec![Beam {
                         current_tile_char,
                         current_tile_index,
                         direction: BeamDirection::North,
-                    }]),
-                    ('\\', BeamDirection::North) => Some(vec![Beam {
+                    }],
+                    ('\\', BeamDirection::North) => vec![Beam {
                         current_tile_char,
                         current_tile_index,
                         direction: BeamDirection::West,
-                    }]),
+                    }],
 
-                    ('|', BeamDirection::West | BeamDirection::East) => Some(vec![
+                    ('|', BeamDirection::West | BeamDirection::East) => vec![
                         Beam {
                             current_tile_char,
                             current_tile_index,
@@ -174,9 +202,9 @@ impl Map {
                             current_tile_index,
                             direction: BeamDirection::South,
                         },
-                    ]),
+                    ],
 
-                    ('-', BeamDirection::North | BeamDirection::South) => Some(vec![
+                    ('-', BeamDirection::North | BeamDirection::South) => vec![
                         Beam {
                             current_tile_char,
                             current_tile_index,
@@ -187,32 +215,17 @@ impl Map {
                             current_tile_index,
                             direction: BeamDirection::West,
                         },
-                    ]),
-                    ('.', BeamDirection::East | BeamDirection::North | BeamDirection::South | BeamDirection::West) => Some(vec![Beam {
+                    ],
+
+                    _ => vec![Beam {
                         current_tile_char,
                         current_tile_index,
                         direction: beam.direction,
-                    }]),
-                    
-                    ('|', BeamDirection::North | BeamDirection::South) => Some(vec![
-                        Beam {
-                            current_tile_char,
-                            current_tile_index,
-                            direction: beam.direction,
-                        }
-                    ]),
-
-                    ('-', BeamDirection::West | BeamDirection::East) => Some(vec![
-                        Beam {
-                            current_tile_char,
-                            current_tile_index,
-                            direction: beam.direction,
-                        }
-                    ]),
-                    _ => panic!()
+                    }],
+                    _ => panic!(),
                 }
             }
-            true => None,
+            true => vec![],
         }
     }
 }
